4bfbe1e4e4a05d42f5951941b288bad553fa411f
advirtys <advirtys2@gmail.com>
2016-02-16 12:41:54 +0600

    Test OrderTest remove method

--- src/test/java/com/epam/pizza/model/OrderTest.java
+++ src/test/java/com/epam/pizza/model/OrderTest.java
@@ -13,11 +13,18 @@ public class OrderTest {
     private Pizza pizza = FactoryPizza.getPizzaById(2);
     private Order order = new Order(user);
     @Test
-    public void testOrder() {
+    public void testOrderAdd() {
         order.setId(1);
         order.add(pizza);
         order.add(pizza);
 
         assertEquals("Order user=1 count=2 price=KZT 3000.00", order.toString());
     }
+
+    @Test
+    public void testOrderRemove() {
+        order.remove();
+        assertEquals("Order user=1 count=0 price=KZT 0.00", order.toString());
+    }
+
 }
