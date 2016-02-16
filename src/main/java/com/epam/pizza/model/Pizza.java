9983bab224d3167e6a6bcd224c4e7eb47e075796
advirtys <advirtys2@gmail.com>
2016-02-16 10:34:11 +0600

    Create Entity

---
+++ src/main/java/com/epam/pizza/model/Pizza.java
@@ -0,0 +1,34 @@
+package com.epam.pizza.model;
+
+import org.joda.money.Money;
+
+
+public class Pizza extends BaseEntity {
+    private String title;
+    private String description;
+    private Money money;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Money getMoney() {
+        return money;
+    }
+
+    public void setMoney(Money money) {
+        this.money = money;
+    }
+}
