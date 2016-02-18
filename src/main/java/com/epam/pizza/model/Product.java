eef4ef04b095c7ed0c247a46bc76fe81cf3cfeff
advirtys <advirtys2@gmail.com>
2016-02-18 11:58:18 +0600

    Add hashCode method for Product

--- src/main/java/com/epam/pizza/model/Product.java
+++ src/main/java/com/epam/pizza/model/Product.java
@@ -31,7 +31,28 @@ public class Product extends BaseEntity {
         this.price = price;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
 
+        Product product = (Product) o;
+
+        if (title != null ? !title.equals(product.title) : product.title != null) return false;
+        if (description != null ? !description.equals(product.description) : product.description != null) return false;
+        return price != null ? price.equals(product.price) : product.price == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        return result;
+    }
 
     @Override
     public String toString() {
